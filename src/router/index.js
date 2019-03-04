418c5d48307bfd107b174bf6ae9405f7860a76ff
jungma <0167739906@naver.com>
2019-03-04 15:47:51 +0900

    study190304

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
 import NotFound from '../components/NotFound.vue'
+import Board from '../components/Board.vue'
+import Card from '../components/Card.vue'
 
 Vue.use(VueRouter)
 
@@ -11,6 +13,9 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/login', component: Login },
+    { path: '/b/:bid', component: Board , children:[
+        {path: 'c/:cid', component:Card}
+    ]},
     { path: '*', component: NotFound }
   ]
 })
